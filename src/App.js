7adc064fd62ed398f966c55db0dfc8dbb44d83b3
hideokamoto <hide.okamoto@digitalcube.jp>
2017-12-13 20:12:36 +0900

    init aws-amplify on react app (all ids are dummy)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+
+Amplify.configure({
+    Auth: {
+        identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab', //REQUIRED - Amazon Cognito Identity Pool ID
+        region: 'XX-XXXX-X', // REQUIRED - Amazon Cognito Region
+        userPoolId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito User Pool ID
+        userPoolWebClientId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito Web Client ID
+    }
+});
 
 class App extends Component {
   render() {
@@ -18,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App);
